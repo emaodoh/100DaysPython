e4fde1ac4bc50ed84fb3a5a5e1a07cf5cb0baf00
emaodoh <bybitonly419@gmail.com>
2026-07-08 09:50:49 +0100

    rockPaperScissors

--- learningSyntax/rockPaperSci.py
+++ learningSyntax/rockPaperSci.py
@@ -2,24 +2,48 @@
 import random
 
 def get_choice():
-    player = input("make a choice from rock,paper or scissors:  ")
-    options = ["rock", "paper", "scissors"]
+    print("Enter q to stop")
+    while True:
+            player = input("make a choice from rock,paper or scissors:  ").lower()
 
-    computer_choice = random.choice(options)
+            if player == "rock" or player == "paper" or player == "scissors":
+                options = ["rock", "paper", "scissors"]
+                computer_choice = random.choice(options)
+                return {"computer": computer_choice, "player": player}
+            if player == "q":
+                return "quit"
 
-    result = {"computer": computer_choice, "player": userInput}
-
-    return result
+            print ("Invalid input (options rock,paper or scissors)")
 
 def check_win(player, computer):
     print(f"you chose {player}, computer chose {computer}")
     if player == computer:
         return "it is a tie"
-    elif player == "rock" and computer == "paper":
-        return "paper cover rock, You loss"
-    elif player == "rock" and computer == "scissors":
-        return "rock smashes scissors: You win! "
+    elif player == "rock":
+        if computer == "paper":
+            return "paper cover rock, You loss"
+        else:
+            return "rock smaches scissors: You win!"
+    elif player == "paper":
+        if computer == "rock":
+            return "paper covers rock: You win!"
+        else:
+            return "scissors cuts paper: You lose"
+    elif player == "scissors":
+        if computer ==   "paper":
+            return "scissors cuts paper: You win!"
+        else:
+            return "rock smaches scissors: You lose"
     else:
-        return "you lose"
-re = check_win("rock", "scissors")
-print(re)
+        return "invalid input"
+
+choices = get_choice()
+if choices == "quit":
+    print ("Thanks for playing the game")
+else:
+    player_choice = choices["player"]
+    computer_choice = choices["computer"]
+
+    result = check_win(player_choice, computer_choice)
+
+    print(result)
