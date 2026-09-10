38c5d1ce58c2df74fd8926bfdeceba26de78bebc
emaodoh <your-real-bybitonly419@gmail.com>
2026-09-10 15:07:25 +0100

    refactor: restructure flask app into package

--- expensesTracker_v2/app.py
+++ expensesTracker_v2/app.py
@@ -1,32 +1,8 @@
-from flask import Flask, render_template
-from config import DevelopmentConfig
+from app import create_app
 
-def create_app():
-
-    app = Flask(__name__)
-
-    app.config.from_object(DevelopmentConfig)
-
-
-    @app.errorhandler(404)
-    def page_not_found(error):
-        return render_template("404.html"), 404
-
-
-    @app.errorhandler(500)
-    def internal_server_error(error):
-        return render_template("500.html"), 500
-
-
-    from routes import main
-    app.register_blueprint(main)
-
-
-    return app
+app = create_app()
 
 
 if __name__ == "__main__":
 
-    app = create_app()
-
-    app.run(debug=False)
+    app.run()
