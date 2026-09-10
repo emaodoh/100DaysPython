d5086918db8f89e3000810acd0797386cddc16b6
emaodoh <your-real-bybitonly419@gmail.com>
2026-09-10 14:13:59 +0100

    refactor: move flask configuration into config class

--- expensesTracker_v2/app.py
+++ expensesTracker_v2/app.py
@@ -1,11 +1,11 @@
 from flask import Flask, render_template
-from config import Config
+from config import DevelopmentConfig
 
 def create_app():
 
     app = Flask(__name__)
 
-    app.config.from_object(Config)
+    app.config.from_object(DevelopmentConfig)
 
 
     @app.errorhandler(404)
