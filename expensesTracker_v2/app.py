d7447233c0449ce6b7c33b0ad776ff2df22111b4
emaodoh <your-real-bybitonly419@gmail.com>
2026-09-10 09:40:26 +0100

    refactor: separate flask routes using blueprint~

--- expensesTracker_v2/app.py
+++ expensesTracker_v2/app.py
@@ -1,84 +1,20 @@
-from flask import Flask, render_template, request, redirect, flash, url_for
-from models import Expense
-import storage 
-import services
-
-expenses = storage.load_expenses()
-
-app = Flask(__name__)
-app.secret_key = "your-secret-key"
-
-@app.route("/")
-def home():
-
-    statistics = services.expense_statistics(expenses)
-
-    return render_template(
-        "index.html",
-        expenses=expenses,
-        statistics=statistics
-    )
-
-@app.route("/edit_expense/<int:id>", methods=["GET", "POST"])
-def edit_expense(id):
-
-    for expense in expenses:
-
-        if expense.new_id == id:
-
-            if request.method == "POST":
-                
-                expense.date = request.form["date"]
-                expense.category = request.form["category"]
-                expense.price = float (request.form["price"])
-                expense.item = request.form["item"]
-
-                storage.save_expenses(expenses)
-                flash("Expense have been updated successfully")
-                return redirect(url_for("home"))
+from flask import Flask
 
 
-            return render_template(
-                "edit.html",
-                expense=expense
-            )
+def create_app():
 
-    return "Expense not found"@app.route("/delete/<int:new_id>")
+    app = Flask(__name__)
 
-@app.route("/delete_expense/<int:id>")
-def delete_expense(id):
-    
-    for expense in expenses:
+    app.secret_key = "your-secret-key"
 
-        if expense.new_id == id:
-            expenses.remove(expense)
-            break
+    from routes import main
+    app.register_blueprint(main)
 
-    storage.save_expenses(expenses)
+    return app
 
-    flash("Expenses deleted successfully")
-
-    return redirect(url_for("home"))
-
-@app.route("/add_expense", methods=["GET", "POST"])
-def add_expense():
-
-    if request.method == "POST":
-
-        category = request.form["category"]
-        item = request.form["item"]
-        price = float (request.form["price"])
-        date = request.form["date"]
-        new_id = len(expenses) +1
-
-        user_expense = Expense(new_id, category, item, str(date), price)
-        expenses.append(user_expense)
-        storage.save_expenses(expenses)
-
-        flash("Expense added successfully!", "success")
-        return redirect(url_for("home"))
-
-    return render_template("add.html")
 
 if __name__ == "__main__":
+
+    app = create_app()
+
     app.run(debug=True)
