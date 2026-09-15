4feb29d1217d080a73ad05f76a8c0a1f1cfdb2ce
emaodoh <your-real-bybitonly419@gmail.com>
2026-09-15 16:28:55 +0100

    refactor: move expense business logic to service layer

--- expensesTracker_v2/app/services.py
+++ expensesTracker_v2/app/services.py
@@ -1,3 +1,9 @@
+from . import storage
+from . import models
+
+expenses = storage.load_expenses()
+
+
 def expense_statistics(expenses):
 
     if not expenses:
@@ -23,3 +29,34 @@ def expense_statistics(expenses):
         "count": count,
         "highest": highest
     }
+
+
+def get_expense_by_id(id):
+    for expense in expenses:
+
+        if expense.new_id == id:
+            return expense
+
+    return None
+
+def update_expense(expense, category, item, price, date):
+    expense.item = item
+    expense.price = price
+    expense.category = category
+    expense.date = date
+
+    return storage.save_expenses(expenses)
+        
+def add_expense(new_id, category, item, date, price):
+    user_expense = models.Expense(new_id, category, item, str(date), price)
+    expenses.append(user_expense)
+    storage.save_expenses(expenses)
+
+
+def delete_expense(id):
+    for expense in expenses:
+        if expense.new_id == id:
+            expenses.remove(expense)
+            break
+    storage.save_expenses(expenses)
+
