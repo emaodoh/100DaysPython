e086ebbefcf80606dad75aaf88024ae7c43bbc4f
emaodoh <your-real-bybitonly419@gmail.com>
2026-09-21 14:23:47 +0100

    basic CRUD operation using linked list

---
+++ Dsa/linkedList/main.py
@@ -0,0 +1,79 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+        current = self.head
+        
+        while current.next:
+            current = current.next
+
+        current.next = new_node
+
+    def print_list(self):
+        current = self.head
+
+        while current:
+            print(current.data)
+            current = current.next
+            
+    def search(self, data):
+        current = self.head
+        
+        while current:
+            if current.data == data:
+                return True
+            
+            current = current.next
+
+        return False
+
+    def delete(self, data):
+        current = self.head
+
+        if not current:
+            return None
+
+        previous = None
+
+        while current:
+            if current.data == data:
+
+                if previous is None:
+                    self.head = current.next
+                else:
+                    previous.next = current.next
+
+                return
+
+                
+                # previous = current.next
+                # self.head = previous
+                # return self.head
+            
+            previous = current
+            current = current.next
+
+
+ll = LinkedList()
+
+ll.append(10)
+ll.append(20)
+ll.append(30)
+print(ll.search(20))
+
+ll.delete(20)
+
+ll.print_list()
